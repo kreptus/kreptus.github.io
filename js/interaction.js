dfbf3babca361d306d99a378ca12812427dd7fcc
3society <esoteric.eso@protonmail.com>
2023-12-24 16:02:06 +0100

    problems

--- js/interaction.js
+++ js/interaction.js
@@ -1,5 +1,19 @@
 function modeswitch() {
-	document.getElementById("background").src = "./img/darkmode.jpg"
+	let image = document.getElementById("background");
+	if(image.src.match("./img/darkmode.jpg")) { 
+		image.src = "./img/lightmode.webp";
+	} else {
+		image.src = "./img/darkmode.jpg";
+	} 
+}
+
+function chBackground() {
+	var container = document.getElementById("background-container");
+	if (container.style.backgroundImage.match("./img/lightmode.webp")) {
+		container.style.backgroundImage = "url('./img/darkmode.jpg')";
+	} else {
+		container.style.backgroundImage = "url('./img/lightmode.webp')";
+	}
 }
 
 function functionName(s) {
