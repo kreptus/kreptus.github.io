18c56e3b2f22f5759873a394aa7c7bd6267baa12
3society <esoteric.eso@protonmail.com>
2023-12-25 04:57:42 +0100

    nav bar

--- js/interaction.js
+++ js/interaction.js
@@ -1,3 +1,5 @@
+const push = 180;
+
 function modeswitch() {
 	let image = document.getElementById("background");
 	if(image.src.match("./img/darkmode.jpg")) { 
@@ -8,11 +10,18 @@ function modeswitch() {
 }
 
 function openNav() {
-	document.getElementById("contents").style.width = "250px";
+	if (document.getElementById("contents").style.width == push + "px") {
+		closeNav();
+	} else {
+	document.getElementById("contents").style.width = push + "px";
+	document.getElementById("main").style.marginLeft = push + "px";
+	}
 }
 
 function closeNav() {
 	document.getElementById("contents").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+
 }
 
 function functionName(s) {
